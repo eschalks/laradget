1e2ab273ea209ce7ded4c1da6682057f3ce392ff
Erwin Schalks <erwinschalks@eppix.com>
2023-03-13 08:42:33 +0100

    Respond with transaction after update

--- app/Http/Controllers/Api/UpdateTransaction.php
+++ app/Http/Controllers/Api/UpdateTransaction.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Data\Api\UpdateTransactionDto;
+use App\Data\Models\TransactionDto;
 use App\Http\Controllers\Controller;
 use App\Models\Transaction;
+use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class UpdateTransaction extends Controller
 {
-    public function __invoke(Transaction $transaction, UpdateTransactionDto $updateTransactionDto): Response
+    public function __invoke(Transaction $transaction, UpdateTransactionDto $updateTransactionDto): Responsable
     {
         \DB::transaction(static function () use ($transaction, $updateTransactionDto) {
             $transaction->update([
@@ -24,6 +26,8 @@ class UpdateTransaction extends Controller
             }
         });
 
-        return response()->noContent();
+        $transaction->loadMissing('counterParty');
+
+        return TransactionDto::from($transaction);
     }
 }
