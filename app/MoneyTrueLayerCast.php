0fa9e7860cb9487c264518941f820dde92ac1036
Erwin Schalks <erwin@schalks.org>
2024-06-05 11:03:58 +0200

    Updated MoneyTrueLayerCast interface implementation

--- app/MoneyTrueLayerCast.php
+++ app/MoneyTrueLayerCast.php
@@ -3,11 +3,12 @@
 namespace App;
 
 use Spatie\LaravelData\Casts\Cast;
+use Spatie\LaravelData\Support\Creation\CreationContext;
 use Spatie\LaravelData\Support\DataProperty;
 
 class MoneyTrueLayerCast implements Cast
 {
-    public function cast(DataProperty $property, mixed $value, array $context): Money
+    public function cast(DataProperty $property, mixed $value, array $properties, CreationContext $context): mixed
     {
         return Money::fromFloat($value);
     }
